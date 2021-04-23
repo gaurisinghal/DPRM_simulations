1dcdafb056728670e377c9d4c6b6bba41a08592d
gaurisinghal <gauri.singhal171@gmail.com>
2021-04-23 20:48:56 +0800

    All monte carlo data added

--- monte_carlo.py
+++ monte_carlo.py
@@ -18,7 +18,7 @@ def monte_carlo_call(iterations, stock = 250, strike = 260, rf= 0.08, q = 0.03, 
 
     #storing expected call payouts to create histogram
     '''
-    with open("expected_call_10000.txt", "a") as filehandle:
+    with open("expected_call_100000.txt", "a") as filehandle:
         filehandle.write(str(expected_call)+"\n")
     '''
 
@@ -37,15 +37,23 @@ def monte_carlo_put(iterations, stock = 250, strike = 260, rf= 0.08, q = 0.03, T
     expected_put = sum(put_payout)/iterations
     discounted_put = expected_put*m.exp(-rf*T) #discounting back to the present
 
+    #storing expected put payouts to create histogram
+    '''
+    with open("Monte Carlo Data/expected_put_10000.txt", "a") as filehandle:
+        filehandle.write(str(expected_put)+"\n")
+    '''
+
+
     return discounted_put
 
-print("call price for 1000 iterations is: "+ str(round(monte_carlo_call(1000),2)))
+print("call price for 1000 iterations is: "+ str(round(monte_carlo_put(1000),2)))
 print("call price for 10,000 iterations is: "+ str(round(monte_carlo_call(10000),2)))
 print("call price for 100,000 iterations is: "+ str(round(monte_carlo_call(100000),2)))
 print("\n")
 print("put price for 1,000 iterations is: "+ str(round(monte_carlo_put(1000),2)))
 print("put price for 10,000 iterations is: "+ str(round(monte_carlo_put(10000),2)))
 print("put price for 100,000 iterations is: "+ str(round(monte_carlo_put(100000),2)))
+
 
 
 
--- monte_carlo_distribution.py
+++ monte_carlo_distribution.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 
-with open("expected_call_10000.txt", "r") as filehandle:
-    text = filehandle.readlines()
+with open("Monte Carlo Data/expected_put_100000.txt", "r") as filehandle: 
+    text = filehandle.readlines() #reading all the expected call/put payouts from the file
 
 
 for i in range(len(text)):
     text[i] = float(text[i].strip())
 
-fig = plt.hist(text) #histogram of expected call payouts
-plt.title("Average(Max(ST-X, 0))")
+fig = plt.hist(text) 
+#plt.title("Average(Max(ST - X, 0))") #histogram of expected call payouts
+plt.title("Average(Max(X - ST, 0))") #histogram of expected put payouts
 plt.show()
